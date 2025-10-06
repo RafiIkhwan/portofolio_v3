a635f723ce31f626a1bc0c56186814c04b1738e2
Rafiikhwan <rafiikhwan2006@gmail.com>
2025-10-06 14:55:46 +0700

    feat: add NavigationMenu component with subcomponents for enhanced navigation UI
    - Implemented NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuTrigger, NavigationMenuContent, NavigationMenuLink, NavigationMenuIndicator, and NavigationMenuViewport components.
    - Integrated Radix UI's Navigation Menu primitives for accessibility and functionality.
    - Added utility function `cn` for conditional class name merging using clsx and tailwind-merge.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
+"use client";
+
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const availableForProjects = true;
+  const jobTitle =  "Backend Engineer";
+  // const mainTitle = "Building end-to-end digital solutions with a focus on robust and scalable system architecture."
+  const description = "Transforming complex problems into efficient code, one line at a time.";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <main className="container mx-auto h-screen overflow-y-hidden flex flex-col justify-between">
+      <div className="flex justify-between items-center p-4 text-2xl font-lora">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink className="text-2xl" href="/">Home</NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="border-green border-2 text-dark-green px-2">
+          <p>{availableForProjects ? "Available for projects" : ""}</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </div>
+      <div className="flex gap-12 justify-between items-end h-fit text-material-black">
+        <div className="relative md:w-1/3 h-fit">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/img/jas_right_crop_larger.png"
+            alt="Profile Picture"
+            width={822}
+            height={1210}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div className="absolute bottom-12 left-0 flex flex-col gap-4 font-poppins">
+            <h1 className="bg-slate-gray text-2xl font-bold tracking-wider">Rafi Ikhwan Purnama</h1>
+            <p className="bg-slate-gray text-xl">{jobTitle}</p>
+          </div>
+        </div>
+        <div className="md:w-2/3 relative p-12">
+          <div className=" flex flex-col gap-4 items-center">
+            <p className="text-8xl md:w-3/4 text-justify">Building <b>end</b>-to-<b>end</b> digital solutions with a focus on robust and <b>scalable</b> system <b>architecture</b>.</p>
+            <p className="text-2xl">{description}</p>
+          </div>
+          <div className="grid grid-cols-6 grid-rows-6 gap-12 absolute -z-10 top-0 right-0 opacity-70">
+            {Array.from({ length: 36 }, (_, i) => (
+              <div 
+                key={i} 
+                className="bg-lightest-gray w-28 h-28"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
